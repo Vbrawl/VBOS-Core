134c412683660420c6ccb1e1f922ac08d5792081
VBrawl <greninjagr@gmail.com>
2022-08-22 11:13:47 -0500

    Now the screen is cleared before showing any text

--- kernel/main.c
+++ kernel/main.c
@@ -5,11 +5,8 @@ int hide_main() {}
 
 
 void main() {
-	//char* vga = (char*)0xb8000;
-	//char c = text[0];
-	//vga[0] = c;
-	//vga[1] = WHITE_ON_BLACK;
-	char* test = "Hello World!";
+
+	vga_clear_screen();
 
 	vga_print_string("Hello World!", WHITE_ON_BLACK);
 }
