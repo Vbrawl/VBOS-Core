55c8506b4af476bcf222a060dcab47618695c726
Vbrawl <vbrawl@localhost.localdomain>
2022-08-24 16:16:15 +0300

    Updated test case

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,5 @@
 #include "../drivers/vga.h"
+#include "../extras/memutils.h"
 
 int hide_main() {}
 
@@ -8,5 +9,8 @@ void main() {
 
 	vga_clear_screen();
 
-	vga_print_string("Hello World!", WHITE_ON_BLACK);
+	for(int i = 0; i < MAX_ROW; i++) {
+		vga_print_string("Hello World!\n\r", WHITE_ON_BLACK);
+	}
+	vga_print_string("AHA\n\r", WHITE_ON_BLACK);
 }
