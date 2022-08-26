3f9ab9db4eac8e863e526549d813831f80a526fc
Vbrawl <vbrawl@localhost.localdomain>
2022-08-26 15:52:01 +0300

    Updated to call all interrupts.

--- kernel/main.c
+++ kernel/main.c
@@ -10,6 +10,14 @@ int hide_main() {}
 void main() {
 	init_idt();
 
-	__asm__ volatile("int $1");
+	__asm__(
+		"int $1;"
+		"int $2;"
+		"int $3;"
+		"int $4;"
+		"int $5;"
+
+
+	);
 	int a = 2/0;
 }
