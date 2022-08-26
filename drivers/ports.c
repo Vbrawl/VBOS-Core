9c9a7f6ba9a2a69ead8fe97320585f037cfbe190
Vbrawl <vbrawl@localhost.localdomain>
2022-08-27 00:21:16 +0300

    Added io_wait().

--- drivers/ports.c
+++ drivers/ports.c
@@ -14,3 +14,9 @@ unsigned char port_bin(unsigned short port) {
 void port_bout(unsigned short port, unsigned char data) {
 	__asm__("out %%al, %%dx":: "a"(data), "d"(port));
 }
+
+
+void io_wait() {
+	// Send garbage to port 0x80 (unused port)
+	port_bout(0x80, 0);
+}
--- drivers/ports.h
+++ drivers/ports.h
@@ -5,6 +5,7 @@
 // port byte in/out
 unsigned char port_bin(unsigned short port);
 void port_bout(unsigned short port, unsigned char data);
+void io_wait();
 
 
 #endif
