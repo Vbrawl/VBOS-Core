5a79324a0701ab189da10336da168eabc78380b3
Vbrawl <vbrawl@localhost.localdomain>
2022-08-30 01:19:03 +0300

    Replaced all handlers with a single (for better OOP integration).

--- cpu/isr.c
+++ cpu/isr.c
@@ -12,11 +12,15 @@
 #include "../drivers/screen.h"
 //#include "../drivers/keyboard.h"
 #include "../extras/types.h"
+#include "../extras/string_funcs.h"
 
 
 extern void* backendisr_Array[];
+isr_handle_holder_t isr_handles;
 
 void init_idt() {
+
+	isr_handles = isr_holder_init();
 
 	for(int i = 0; i < 256; i++) {
 		idt_set_entry(i, (uintptr_t)backendisr_Array[i]);
@@ -30,360 +34,50 @@ void init_idt() {
 }
 
 
-
-
-
-/**********************
-* Common Null Handler *
-**********************/
-void isr_null(unsigned char idt_code) {
-	screen_write(&VGA_SCREEN, "ERROR: ISR Null Handler\n\r", true);
-
-	#warning *** NULL handler is configured to freeze everything, You may want to change that ***
-	//__asm__ volatile("hlt");
-}
-
-
-/*******************
-* Division By Zero *
-*******************/
-void isr_0() {
-	screen_write(&VGA_SCREEN, "ERROR: Division By Zero\n\r", true);
-
-	#warning *** You may want to change this action (DivBy0) ***
-//	__asm__ volatile("cli; hlt");
-}
-
-
-/********
-* Debug *
-********/
-void isr_1() {
-	screen_write(&VGA_SCREEN, "ERROR: Debug\n\r", true);
-
-	#warning *** Add an action for Debug (Debug) ***
-}
-
-/***************
-* Non-Maskable *
-****************/
-void isr_2() {
-	screen_write(&VGA_SCREEN, "ERROR: Non-Maskable\n\r", true);
-
-	#warning *** Add an action for Non-Maskable ***
-}
-
-/*************
-* Breakpoint *
-*************/
-void isr_3() {
-	screen_write(&VGA_SCREEN, "ERROR: Breakpoint\n\r", true);
-
-	#warning *** Add an action for Breakpoint ***
-}
-
-/*************************
-* Into Detected Overflow *
-*************************/
-void isr_4() {
-	screen_write(&VGA_SCREEN, "ERROR: Into Detected Overflow\n\r", true);
-
-	#warning *** Add an action for Into Detected Overflow ***
-}
-
-/****************
-* Out of Bounds *
-****************/
-void isr_5() {
-	screen_write(&VGA_SCREEN, "ERROR: Out of Bounds\n\r", true);
-
-	#warning *** Add an action for Out of Bounds ***
-}
-
-
-/*****************
-* Invalid OpCode *
-*****************/
-void isr_6() {
-	screen_write(&VGA_SCREEN, "ERROR: Invalid OpCode\n\r", true);
-
-	#warning *** Add an action for Invalid OpCode ***
-}
-
-
-/*****************
-* No Coprocessor *
-*****************/
-void isr_7() {
-	screen_write(&VGA_SCREEN, "ERROR: No Coprocessor\n\r", true);
-
-	#warning *** Add an action for No Coprocessor ***
-}
-
-/***************
-* Double Fault *
-***************/
-void isr_8() {
-	screen_write(&VGA_SCREEN, "ERROR: Double Fault\n\r", true);
-
-	#warning *** Add an action for Double Fault ***
-}
-
-/******************************
-* Coprocessor Segment Overrun *
-******************************/
-void isr_9() {
-	screen_write(&VGA_SCREEN, "ERROR: Coprocessor Segment Overrun\n\r", true);
-
-	#warning *** Add an action for Coprocessor Segment Overrun ***
-}
-
-
-/**********
-* Bad TSS *
-**********/
-void isr_10() {
-	screen_write(&VGA_SCREEN, "ERROR: Bad TSS\n\r", true);
-
-	#warning *** Add an action for Bad TSS ***
-}
-
-/**********************
-* Segment Not Present *
-**********************/
-void isr_11() {
-	screen_write(&VGA_SCREEN, "ERROR: Segment Not Present\n\r", true);
-
-	#warning *** Add an action for Segment Not Present ***
-}
-
-/**************
-* Stack Fault *
-**************/
-void isr_12() {
-	/********************************************
-	* Possible Solution:                        *
-	*                                           *
-	* Go to a new location and:                 *
-	*                                           *
-	* > safe_memcpy(old_stack, new_location)    *
-	* then set the stack bs to new_location     *
-	* and stack sp to new_location - stack_size *
-	********************************************/
-	screen_write(&VGA_SCREEN, "ERROR: Stack Fault\n\r", true);
-
-	#warning *** Add an action for Stack Fault ***
-}
-
-/***************************
-* General Protection Fault *
-***************************/
-void isr_13() {
-	screen_write(&VGA_SCREEN, "ERROR: General Protection Fault\n\r", true);
-
-	#warning *** General Protection Fault handler is configured to freeze the machine. ***
-	__asm__ volatile("hlt");
-}
-
-/*************
-* Page Fault *
-*************/
-void isr_14() {
-	screen_write(&VGA_SCREEN, "ERROR: Page Fault\n\r", true);
-
-	#warning *** Add an action for Page Fault ***
-}
-
-/****************
-* Reserved (15) *
-****************/
-void isr_15() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (15)\n\r", true);
-
-	#warning *** No action for Reserved (15) ***
-}
-
-/*****************
-* Floating Point *
-*****************/
-void isr_16() {
-	screen_write(&VGA_SCREEN, "ERROR: Floating Point\n\r", true);
-
-	#warning *** Add an action for Floating Point ***
-}
-/******************
-* Alignment Check *
-******************/
-void isr_17() {
-	screen_write(&VGA_SCREEN, "ERROR: Alignment Check\n\r", true);
-
-	#warning *** Add an action for Alignment Check ***
-}
-
-/****************
-* Machine Check *
-****************/
-void isr_18() {
-	screen_write(&VGA_SCREEN, "ERROR: Machine Check\n\r", true);
-
-	#warning *** Add an action for Machine Check ***
-}
-
-/****************
-* Reserved (19) *
-****************/
-void isr_19() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (19)\n\r", true);
-
-	#warning *** No action for Reserved (19) ***
-}
-
-/****************
-* Reserved (20) *
-****************/
-void isr_20() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (20)\n\r", true);
-
-	#warning *** No action for Reserved (20) ***
-}
-
-/****************
-* Reserved (21) *
-****************/
-void isr_21() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (21)\n\r", true);
-
-	#warning *** No action for Reserved (21) ***
-}
-
-/****************
-* Reserved (22) *
-****************/
-void isr_22() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (22)\n\r", true);
-
-	#warning *** No action for Reserved (22) ***
-}
-
-/****************
-* Reserved (23) *
-****************/
-void isr_23() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (23)\n\r", true);
-
-	#warning *** No action for Reserved (23) ***
-}
-
-/****************
-* Reserved (24) *
-****************/
-void isr_24() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (24)\n\r", true);
-
-	#warning *** No action for Reserved (24) ***
-}
-
-/****************
-* Reserved (25) *
-****************/
-void isr_25() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (25)\n\r", true);
-
-	#warning *** No action for Reserved (25) ***
-}
-
-/****************
-* Reserved (26) *
-****************/
-void isr_26() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (26)\n\r", true);
-
-	#warning *** No action for Reserved (26) ***
-}
-
-/****************
-* Reserved (27) *
-****************/
-void isr_27() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (27)\n\r", true);
-
-	#warning *** No action for Reserved (27) ***
-}
-
-/****************
-* Reserved (28) *
-****************/
-void isr_28() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (28)\n\r", true);
-
-	#warning *** No action for Reserved (28) ***
-}
-
-/****************
-* Reserved (29) *
-****************/
-void isr_29() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (29)\n\r", true);
-
-	#warning *** No action for Reserved (29) ***
-}
-
-/****************
-* Reserved (30) *
-****************/
-void isr_30() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (30)\n\r", true);
-
-	#warning *** No action for Reserved (30) ***
-}
-
-/****************
-* Reserved (31) *
-****************/
-void isr_31() {
-	screen_write(&VGA_SCREEN, "ERROR: Reserved (31)\n\r", true);
-
-	#warning *** No action for Reserved (31) ***
+void main_isr_handler(unsigned char idt_code) {
+
+	char hex_code[10];
+	int_to_hexstring(idt_code, hex_code, 10);
+	screen_write(&VGA_SCREEN, "Interrupt: ", false);
+	screen_write(&VGA_SCREEN, hex_code, false);
+	screen_write(&VGA_SCREEN, "\n\r", false);
+
+
+	for(int i = 0; i < ISR_SIGNAL_CATEGORY_SIZE; i++) {
+		isr_handler_ptr_t* ptr = isr_handles.isr_signals[idt_code].ptrs[i];
+		if(ptr != 0) {
+			ptr(idt_code);
+		}
+	}
 }
 
 
 
 
+isr_handle_holder_t isr_holder_init() {
 
-/*************
-* Extra ISRs *
-*************/
+	isr_handle_holder_t _this;
 
+	for(int i = 0; i < 256; i++) {
+		for(int o = 0; o < ISR_SIGNAL_CATEGORY_SIZE; o++) {
+			_this.isr_signals[i].ptrs[o] = 0;
+		}
+	}
 
-/***********
-* Free ISR *
-***********/
-void isr_32() {
-	screen_write(&VGA_SCREEN, "ERROR: Free ISR (32)\n\r", false);
-
-	#warning *** No action for free ISR (32) ***
+	return _this;
 }
 
-/***********
-* Keyboard *
-***********/
-void isr_33() {
-        screen_write(&VGA_SCREEN, "ERROR: Keyboard Interrupt (33)\n\r", false);
 
-	//keyboard_notify();
-
-        #warning *** No action for free ISR (33) ***
-
-	pic_accept(false);
-
-	screen_write(&VGA_SCREEN, "after\n\r", false);
+void isr_holder_register_handler(isr_handle_holder_t* _this, isr_handler_ptr_t* ptr, size_t category, size_t position) {
+	_this->isr_signals[category].ptrs[position] = ptr;
 }
 
-/***********
-* Free ISR *
-***********/
-void isr_34() {
-        screen_write(&VGA_SCREEN, "ERROR: Free ISR (34)\n\r", false);
 
-        #warning *** No action for free ISR (34) ***
+void isr_holder_install(isr_handle_holder_t* _this) {
+	for(int i = 0; i < 256; i++) {
+		for(int o = 0; o < ISR_SIGNAL_CATEGORY_SIZE; o++) {
+			isr_handles.isr_signals[i].ptrs[o] = _this->isr_signals[i].ptrs[o];
+		}
+	}
 }
+
--- cpu/isr.h
+++ cpu/isr.h
@@ -1,62 +1,39 @@
 #ifndef ISR_H
 #define ISR_H
 
+#include "../extras/types.h"
+
 
 #define MASTER_PIC_OFFSET	0x20 //32
 #define SLAVE_PIC_OFFSET	0x28 //39
 
 
+#define ISR_KEYBOARD		33
+
+
+#define ISR_SIGNAL_CATEGORY_SIZE 5
+
+typedef void(isr_handler_ptr_t)(unsigned char);
+
+typedef struct {
+	isr_handler_ptr_t* ptrs[ISR_SIGNAL_CATEGORY_SIZE];
+} isr_signal_category_t;
+
+typedef struct {
+	isr_signal_category_t isr_signals[256];
+} isr_handle_holder_t;
+
+
+// isr_handle_holder_t isr_handles;
+
 
 void init_idt();
-void isr_null(unsigned char idt_code);
+void main_isr_handler(unsigned char idt_code);
 
 
-void isr_0(); // Division By Zero
-void isr_1(); // Debug
-void isr_2(); // Non-Maskable
-void isr_3(); // Breakpoint
-void isr_4(); // Into Detected Overflow
-void isr_5(); // Out of Bounds
-void isr_6(); // Invalid OpCode
-void isr_7(); // No Coprocessor
-void isr_8(); // Double Fault
-void isr_9(); // Coprocessor Segment Overrun
-void isr_10(); // Bad TSS
-void isr_11(); // Segment Not Present
-void isr_12(); // Stack Fault
-void isr_13(); // General Protection Fault
-void isr_14(); // Page Fault
-
-void isr_16(); // Floating Point
-void isr_17(); // Alignment Check
-void isr_18(); // Machiine Check
-
-
-/***********
-* Reserved *
-***********/
-void isr_15();
-
-void isr_19();
-void isr_20();
-void isr_21();
-void isr_22();
-void isr_23();
-void isr_24();
-void isr_25();
-void isr_26();
-void isr_27();
-void isr_28();
-void isr_29();
-void isr_30();
-void isr_31();
-
-/*************
-* Extra ISRs *
-*************/
-void isr_32();
-void isr_33();
-void isr_34();
+isr_handle_holder_t isr_holder_init();
+void isr_holder_register_handler(isr_handle_holder_t* _this, isr_handler_ptr_t ptr, size_t category, size_t position);
+void isr_holder_install(isr_handle_holder_t* _this);
 
 
 #endif
